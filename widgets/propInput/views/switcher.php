7369abbf71353ae89098bd25d9f9fe4dcceacb65
Boris <fenixq@mail.ru>
2020-03-06 22:30:26 +0300

    refactoring

--- widgets/propInput/views/switcher.php
+++ widgets/propInput/views/switcher.php
@@ -10,5 +10,19 @@ if ($model->value === null) {
     $model->value = (int) $model->prop->default_value;
 }
 
-echo HTML::label($model->prop->title);
-echo Html::activeRadioList($model, $attributeName, [0=> 'Нет', 1 => 'Да']);
+//echo HTML::label($model->prop->title);
+//echo Html::activeRadioList($model, $attributeName, [0=> 'Нет', 1 => 'Да']);
+
+echo '<br />' . HTML::activeRadioList($model, $attributeName, [0 => 'Нет', 1 => 'Да'], [
+        'radioTemplate' => '{input}',
+        'item' => function ($index, $label, $name, $checked, $value) {
+            $checked = ($checked) ? 'checked' : '';
+            $active = ($checked) ? 'active' : '';
+            $return = '<label class="btn btn-default ' . $active . '">';
+            $return .= '<input type="radio" name="' . $name . '" value="' . $value . '" ' . $checked . '>' . ucwords($label);
+            $return .= '</label>';
+            return $return;
+        },
+        'class' => 'btn-group',
+        'data-toggle' => 'buttons',
+    ]);

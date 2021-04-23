02f7a84ac3b63a82504023026296dfc1e51af737
Boris <fenixq@mail.ru>
2021-04-23 12:44:25 +0300

    composer dep

--- Module.php
+++ Module.php
@@ -17,42 +17,6 @@ class Module extends \yii\base\Module
     {
         parent::init();
         BlockAsset::register(\Yii::$app->view);
-
-        // Kartik reset
-        \Yii::$app->params['bsDependencyEnabled'] = false; // do not load bootstrap assets for a specific asset bundle
-        \Yii::$app->params['bsVersion'] = '4.x';
-        \Yii::$app->assetManager->bundles['kartik\select2\ThemeKrajeeBs4Asset'] = [
-            'css' => [],
-        ];
-
-//        \Yii::$app->assetManager->appendTimestamp = true;
-//        \Yii::$app->assetManager->bundles = [
-////            'yii\web\JqueryAsset' => [
-//////                'js' => [
-//////                    '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
-//////                ],
-////                'jsOptions' => [
-////                    'position' =>  View::POS_HEAD,
-////                ],
-////            ],
-////            'yii\bootstrap\BootstrapAsset' => [
-////                'css' => [],
-////            ],
-////            'yii\bootstrap\BootstrapThemeAsset' => [
-////                'css' => [],
-//////                'css' => [], // ??
-////            ],
-//
-//
-////            'yii\bootstrap4\BootstrapAsset' => [
-////                'css' => [],
-////            ],
-////            'yii\bootstrap4\BootstrapPluginAsset' => [
-////                'js' => [],
-//////                'css' => [], // ??
-////            ],
-//
-//        ];
     }
 
 //    /**
